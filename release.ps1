5623acd50273fc8a54afa7a9801b8d3fb76530c7
Zarainia <fir.dakk.del@gmail.com>
2022-11-19 09:55:44 -0500

    Added icon processing for android and made script not hang waiting for input on actions

--- release.ps1
+++ release.ps1
@@ -3,4 +3,4 @@ flutter pub run flutter_launcher_name:main
 flutter pub run flutter_launcher_icons
 flutter build windows --no-sound-null-safety
 Invoke-WebRequest "https://github.com/tekartik/sqflite/raw/master/sqflite_common_ffi/lib/src/windows/sqlite3.dll" -OutFile "build/windows/runner/Release/sqlite3.dll"
-flutter pub run msix:create
+flutter pub run msix:create --install-certificate false
