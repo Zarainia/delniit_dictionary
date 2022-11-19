3f1a5c6734aa8ffaa9009e0387669dd19899072b
Zarainia <fir.dakk.del@gmail.com>
2022-11-19 10:09:49 -0500

    Try to fix errors from missing platforms

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 flutter pub run change_app_package_name:main com.valliyv.delniit_dictionary
 flutter pub run flutter_launcher_name:main
-flutter pub run flutter_launcher_icons
+flutter pub run flutter_launcher_icons -f windows_config.yaml
 flutter build windows --no-sound-null-safety
 Invoke-WebRequest "https://github.com/tekartik/sqflite/raw/master/sqflite_common_ffi/lib/src/windows/sqlite3.dll" -OutFile "build/windows/runner/Release/sqlite3.dll"
 flutter pub run msix:create --install-certificate false
