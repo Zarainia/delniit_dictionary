405707fc69dd183a242cf3f5d56be1e5b074f3e3
Zarainia <fir.dakk.del@gmail.com>
2022-11-19 16:24:53 -0500

    Switch to ``-AsByteStream`

--- release.ps1
+++ release.ps1
@@ -5,5 +5,5 @@ flutter build windows --release --no-sound-null-safety
 Invoke-WebRequest "https://github.com/tekartik/sqflite/raw/master/sqflite_common_ffi/lib/src/windows/sqlite3.dll" -OutFile "build/windows/runner/Release/sqlite3.dll"
 $pfx_path = "windows/cert.pfx";
 $decoded_bytes = [System.Convert]::FromBase64String($env:BASE64_PFX);
-Set-Content $pfx_path -Value $decoded_bytes -Encoding Byte;
+Set-Content $pfx_path -Value $decoded_bytes -AsByteStream;
 flutter pub run msix:create --install-certificate false --certificate-password $env:CERT_PASSWORD
